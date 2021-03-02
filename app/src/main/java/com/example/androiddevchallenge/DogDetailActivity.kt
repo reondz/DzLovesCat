f43cb5fd207278e5e3db2f686d29aaa18e18547c
daizheng.l <daizheng.l@bytedance.com>
2021-03-02 15:56:29 +0800

    fix lint

--- app/src/main/java/com/example/androiddevchallenge/DogDetailActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/DogDetailActivity.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.model.DogBrief
 import com.example.androiddevchallenge.model.getString
-import com.example.androiddevchallenge.ui.theme.MyTheme
 import dev.chrisbanes.accompanist.glide.GlideImage
 
 class DogDetailActivity : AppCompatActivity() {
